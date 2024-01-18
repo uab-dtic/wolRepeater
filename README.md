182ede584f2bad7eedf4bd60d7ccb6918e086682
Jordi Román Mejias <Jordi.Roman@uab.cat>
2024-01-18 15:50:47 +0100

    1st version

---
+++ README.md
@@ -0,0 +1,203 @@
+# wolRepeater
+
+Un simple script clonado de <https://github.com/jaime29010/wol-packet-replicator> y un poco modificado.
+
+Esta es una forma fàcil de usar un WakeOnLan desde fuera de la red _cuando tu router no soporta el reenvio de paquetes_ a la direccion de broadcast
+
+El script tambien soporta la feature SecureOn si la NIC no la soporta
+
+## Dependencias
+
+Para crear un entono virtual de python necesitamos los paquetes
+
+- python3-venv  : para crear el entorno virtuak
+- python3-dev   : para construir las dependencias dentro del entorno virtual
+  
+Para ejecutar el script necesitamos la dependencia
+
+- jsonschema    : para checkear el fichero json de la configuracion
+
+## instalación/configuración
+
+La configuracioón se puede realizar con cualquier usuario que tenga permisos para escribir en el directorio de destino.
+
+La ejecución tambien se puede realizar por cualquier usuario.
+
+Direcciones del repositorio:
+
+- https : https://github.com/uab-dtic/wolRepeater.git
+- ssh   : git@github.com:uab-dtic/wolRepeater.git
+
+**Vamos a suponer que tenemos el codigo en el directorio /opt/wolRepeater.**
+
+Si descargamos desde un repositorio de git lo podemos hacer directamente con el comando
+
+```bash
+git clone {REPOSITORY} /opt/wolRepeater
+```
+
+Creamos el entorno virtual
+
+```bash
+cd /opt/wolrepeater
+python -m venv env
+. env/bin/activate
+```
+
+En este punto en el prompt debemos ver que estamos en un entorno virtual
+
+```data
+(env) root@hostname:/opt/wolRepeater #
+```
+
+Añadimos las dependencias
+
+```bash
+(env) root:/opt/wolRepeater # pip install -r requirements.txt
+```
+
+## Forma de uso desde linea de comandos
+
+Si necesitamos que el entorno virtual quede persistente necesitamos activarlo
+
+```bash
+usuario@localhost:~$ cd /opt/wolRepeater
+usuario@localhost:/opt/wolRepeater$ . env/bin/activate
+(env) usuario@localhost:/opt/wolRepeater$ ./wolRepeater -h
+...
+```
+
+Si queremos ejecutarlo **sin un entorno virtual persistente**
+
+```bash
+
+usuario@localhost:~ # /opt/wolRepeater/env/bin/python /opt/wolRepeater/wolReperater.py -h
+...
+
+```
+
+## Opciones de uso
+
+./wolRepeater.py [-h] [-i ip] [-p port] [-t ip] [-r port] [-s password] [-z password] [-f mac_and_passwd_json_file] [-l log file]
+
+   -h show this help.
+   -i binding ip. Default 0.0.0.0.
+   -p binding port. Default 5009.
+   -t target ip. Default 255.255.255.255.
+   -r target port. Default 9.
+   -f json file with mac's and passwords. View format down.
+   -s password for use on forward packet. Default ''.
+   -z password to test on received packets. Default ''.
+
+Formato del fichero json
+
+```json
+[
+  { "ethernet": "112233445566", "password": "aabbccddeeff",
+  { "ethernet": "aa2233445566", "password": "11bbccddeeff"  
+]
+```
+
+Environment Variables:
+   LOGLEVEL=[DEBUG|INFO|WARNING|ERROR|CRITICAL] default=INFO
+
+Examples:
+
+```data
+     ./wolRepeater.py -h
+     ./wolRepeater.py -i 192.168.1.100
+     ./wolRepeater.py -p 8000
+     ./wolRepeater.py -t 192.168.1.100
+     ./wolRepeater.py -r 8000
+     ./wolRepeater.py -s 112233445566
+     ./wolRepeater.py -f /etc/wol/mac_and_pass.json
+     ./wolRepeater.py -z aabbccddeeff
+     ./wolRepeater.py -l /var/log/wol_wolRepeater.log
+     LOGLEVEL=DEBUG ./replicator.py
+```
+
+## Forma de uso desde SystemD
+
+Copiar o hacer un link del fichero wolRepeater.service a /etc/systemd/system/
+
+```bash
+sudo ln -s /opt/wolRepeater/wolRepeater.service /etc/systemd/system/wolRepeater.service
+```
+el servicio se arrancará con el usuario wolrepeater que debemos crear
+
+```bash
+sudo adduser --system --no-create-home --home /opt/wolrepeater --shell /usr/sbin/nologin wolrepeater
+```
+
+Modificar el fichero **/opt/wolRepeater/wolRepeater.conf** con las opciones apropiadas
+
+***OJO!!!** si se usa la opcion -l para generar un fichero de log en /var/log/wolRepeater.log, este fichero **DEBE** ser propiedad del usuario que arranca el servicio (_wolrepeater_)
+
+```bash
+sudo touch /var/log/wolRepeater.log
+sudo chown wolrepeater /var/log/wolRepeater.log
+```
+
+Habilitar el servicio para que arranque tras un reinicio
+
+```bash
+sudo systemctl enable wolRepeater
+```
+
+Arrancar el servicio manualmente
+
+```bash
+sudo systemctl start wolRepeater
+```
+
+Comprobar el estadodel servicio
+
+```bash
+sudo systemctl status wolRepeater
+```
+
+## configuraciones adicionales
+
+Hay que tener en cuenta el fichero de log que se genere y configurar el logrotate para que no se llene el disco inecesariamente
+
+Por ejemplo podemos añadir el siguiente **/etc/logrotate.d/wolRepeater**
+
+```data
+/var/log/wolRepeater.log {
+
+    daily
+    rotate 2
+    compress
+
+    delaycompress
+    missingok
+
+    create 0640 wolrepeater root
+
+    postrotate
+        systemctl restart wolRepeater
+    endscript
+}
+```
+
+## Daisy Chain use
+
+Podemos usar el script en cadena. Unservidor se configura para enviar paquetes con  _secureOn_ y un segundo servidor para recibirlos con  _secureOn_.
+
+```mermaid
+graph LR
+  WS[WoL Sender]
+
+  WS-->RSS
+
+  RSS[Wol Repeater \nsend with SecureOn]
+
+    RSS === RSR[SecureOn Enabled]
+
+  RSR[Wol Repeater\nreceive with SecureOn]
+    
+    RSR --> MA
+
+  MA[Final Machines]
+
+```
---
+++ wolRepeater.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python
+
+import binascii
+import re
+import socket
+import sys
+import logging
+import os
+import getopt
+import json
+import jsonschema
+
+# https://regex101.com/r/2l8eJp/3
+DGRAM_REGEX = re.compile(r'(?:^([fF]{12})(([0-9a-fA-F]{12}){16})([0-9a-fA-F]{12})?$)')
+ETHER_REGEX = re.compile( r'^[0-9a-fA-F]{12}$')
+
+BIND_ADDRESS = "0.0.0.0"
+BIND_PORT = 9999
+
+TARGET_ADDRESS = "255.255.255.255"
+TARGET_PORT = 9
+
+
+MAC_PASS_FILE = ""
+
+FORCED_AUTH_HOSTS = {}
+# Password to send on forward packets
+FORWARD_PASS = ""
+# Password to check on received packets
+GLOBAL_PASS = ""
+
+LOG_FILE = ""
+
+def is_allowed(address, password):
+    if address in FORCED_AUTH_HOSTS:
+        logger.debug("Enforcing authentication for {} : {} <-> {}". format(address, FORCED_AUTH_HOSTS[address], password) )
+        return password == FORCED_AUTH_HOSTS[address]
+    elif GLOBAL_PASS != "":
+        logger.debug("Enforcing Global authentication for {} : {} <-> {}". format(address, GLOBAL_PASS, password) )
+        return password == GLOBAL_PASS
+    return True
+
+def forward_packet(data):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.connect((TARGET_ADDRESS, TARGET_PORT))
+    sock.send(data)
+    sock.close()
+
+
+def handle_packet(data):
+    payload = binascii.hexlify(data).decode('utf-8')
+    logger.debug("Received payload: %s" % payload)
+
+    if DGRAM_REGEX.match(payload):
+        search = DGRAM_REGEX.search(payload)
+        address = search.group(3)
+        password = search.group(4)
+
+        if is_allowed(address, password):
+            logger.debug("Forwarding the packet for %s to %s:%s" % (address, TARGET_ADDRESS, TARGET_PORT))
+            if ( FORWARD_PASS != "" ):
+                newPayload = search.group(1) + search.group(2) + FORWARD_PASS
+                logger.debug( "New payload     : {}".format(payload) )
+                data = binascii.unhexlify( newPayload )
+            #
+            forward_packet(data)
+        else:
+            logger.debug("This request has been denied because the received password is not correct")
+    else:
+        logger.debug("Received payload is not valid, ignoring...")
+
+
+def start_listener():
+    logger.debug( "Start_listener on {}:{}".format( BIND_ADDRESS, BIND_PORT ))
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((BIND_ADDRESS, BIND_PORT))
+
+    while True:
+        data, addr = sock.recvfrom(108)
+        logger.debug("Received packet from %s:%s" % (addr[0], addr[1]))
+        handle_packet(data)
+
+
+def show_help( error=0 ):
+    logger.debug( "show_help")
+    print( 
+        "wolRepeater", 
+        "Forma de uso:",
+        "    {} [-h] [-i ip] [-p port] [-t ip] [-r port] [-s password] [-z password] [-f mac_and_passwd_json_file] [-l log file] [-v loglevel]".format( __file__ ),
+        "",
+        "   -h show this help.",
+        "   -i binding ip. Default {}.".format(BIND_ADDRESS),
+        "   -p binding port. Default {}.".format(BIND_PORT),
+        "   -t target ip. Default {}.".format(TARGET_ADDRESS) ,
+        "   -r target port. Default {}.".format(TARGET_PORT),
+        "   -f json file with mac's and passwords. View format down.",
+        "   -s password for use on forward packet. Default ''.",
+        "   -l file log",
+        "   -z password to test on received packets. Default ''.",
+        "",
+        "json format file:",
+        "",
+        "[",
+        "  { \"ethernet\": \"112233445566\", \"password\": \"aabbccddeeff\", ",
+        "  { \"ethernet\": \"aa2233445566\", \"password\": \"11bbccddeeff\"  ",
+        "]",
+        "",
+        "Variables de entorno:",
+        "   LOGLEVEL=[DEBUG|INFO|WARNING|ERROR|CRITICAL] default=INFO",
+        "",
+        "Examples:",
+        "     {} -h ".format( __file__ ),
+        "     {} -i 192.168.1.100".format( __file__ ),
+        "     {} -p 8000".format( __file__ ),
+        "     {} -t 192.168.1.100".format( __file__ ),
+        "     {} -r 8000".format( __file__ ),
+        "     {} -s 112233445566".format( __file__ ),
+        "     {} -f /etc/wol/mac_and_pass.json".format( __file__ ),
+        "     {} -z aabbccddeeff".format(__file__ ),
+        "     {} -l /var/log/wol_replicator.log".format( __file__ ),
+        "     {} -v DEBUG".format( __file__ ),
+        "     LOGLEVEL=DEBUG {}".format(__file__),
+        "",
+        sep = "\n"
+        )
+    sys.exit( error )
+    #
+
+def json_validation ( data ):
+
+    logger.debug( "Data: '{}'".format(data))
+
+    schema = {
+
+            "type" : "array",
+            "items" : {
+                "type" : "object",
+                "properties" : {
+                    "ethernet": {"type" : "string", "pattern": "^[0-9a-fA-F]{12}$"},
+                    "password": {"type" : "string", "pattern": "^[0-9a-fA-F]{12}$"}
+                                },
+                "required": ["ethernet","password"]
+            }
+
+          }
+    try :
+        jsonschema.validate(instance=data, schema=schema)
+
+    except jsonschema.exceptions.ValidationError as v:
+            logger.critical ( v )
+            sys.exit(1)
+    else:
+        logger.info( "JSON File validated.")
+
+    # Convert json validated to simple dictionary
+
+    TMP_HOSTS = {}
+
+    for item in data:
+        logger.debug ("secured mac : {} : {}".format( item['ethernet'], item['password'] ))
+        TMP_HOSTS[ item['ethernet'] ] = item['password']
+    #
+    logger.debug( "Authorized Ethernets: '{}'".format( TMP_HOSTS ))
+    return TMP_HOSTS
+#
+
+if __name__ == '__main__':
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)s] - %(message)s")
+    logger = logging.getLogger()
+
+    LOGLEVEL = os.environ.get('LOGLEVEL','INFO')
+    
+    logger.setLevel(LOGLEVEL)
+
+    log = logging.getLogger( __name__ )
+
+    consoleHandler = logging.StreamHandler(sys.stdout)
+    consoleHandler.setFormatter(logFormatter)
+    logger.addHandler(consoleHandler)
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],'hi:p:f:l:s:t:r:v:')
+    except getopt.GetoptError:
+        logger.critical( "Error en las opciones" )
+        show_help( 2 )
+    
+    logger.debug( "opts: {}".format( opts))
+    logger.debug( "args: {}".format( args))
+
+    for opt, arg in opts:
+        if opt == '-h':
+            show_help( 0 )
+
+        elif opt == '-i':
+            BIND_ADDRESS = arg
+            logger.debug( "New BIND Address:{}".format( BIND_ADDRESS ))
+
+        elif opt == '-p':
+            BIND_PORT = int( arg )
+            logger.debug( "New BIND Port:{}".format( BIND_PORT ))
+
+        elif opt == '-t':
+            TARGET_ADDRESS = arg
+            logger.debug( "New TARGET Address:{}".format( TARGET_ADDRESS ))
+
+        elif opt == '-r':
+            TARGET_PORT = int( arg )
+            logger.debug( "New TARGET Port:{}".format( TARGET_PORT ))
+
+        elif opt == '-s':
+            if ETHER_REGEX.match(arg):
+                FORWARD_PASS = arg
+                logger.debug( "FORWARD_PASS: '{}' like ethernet 'aabbccddeeff' ".format(FORWARD_PASS))
+            else:
+                logger.critical( "Invalid FORWARD_PASS: '{}', ethernet like needed 'aabbccddeeff' ".format(arg))
+                sys.exit (1)
+            #
+
+        elif opt == "-f":
+            MAC_PASS_FILE = arg
+            logger.debug( "File mac and passwords: {}".format( MAC_PASS_FILE ))
+
+            if  not os.path.isfile( MAC_PASS_FILE ) :
+                logger.critical ("{} Not found.".format( MAC_PASS_FILE ))
+                sys.exit()
+            #
+            json_content=""
+            try :
+                with open( MAC_PASS_FILE ) as json_file:
+                    json_content = json.load(json_file)
+                #
+            #    
+            except Exception as e:
+                log.critical("Error al usar el fichero : '{}' {}".format( MAC_PASS_FILE, type(e).__name__ ))
+                sys.exit(1)
+
+            FORCED_AUTH_HOSTS = json_validation ( json_content )
+
+        elif opt == '-z':
+            if ETHER_REGEX.match(arg):
+                GLOBAL_PASS = arg
+                logger.debug( "GLOBAL_PASS: '{}' like ethernet 'aabbccddeeff' ".format(GLOBAL_PASS))
+            else:
+                logger.critical( "Invalid GLOBAL_PASS: '{}', ethernet like needed 'aabbccddeeff' ".format(arg))
+                sys.exit (1)
+            #
+        elif opt == "-l":
+            LOG_FILE = arg
+            logger.debug( "Log File:{}".format( LOG_FILE ))
+
+            fileHandler = logging.FileHandler( LOG_FILE )
+            fileHandler.setFormatter(logFormatter)
+            logger.addHandler(fileHandler)
+
+        elif opt == '-v':
+            LOGLEVEL = arg
+            try :
+                logger.setLevel(LOGLEVEL)
+                logger.debug( "Set new LOGLEVEL:{}".format( LOGLEVEL ))
+            except:
+                logger.critical( "LOGLEVEL ERROR ON ID '{}'".format(LOGLEVEL))
+                show_help(3)
+            #
+        #
+        #
+    #
+    
+    # Show configuration by default
+    logger.info( "BIND Address:{}".format( BIND_ADDRESS ))
+    logger.info( "BIND Port:{}".format( BIND_PORT ))
+    logger.info( "TARGET Port:{}".format( TARGET_PORT ))
+    logger.debug( "FORWARD_PASS: '{}' like ethernet 'aabbccddeeff' ".format(FORWARD_PASS))
+    logger.info( "File mac and passwords: {}".format( MAC_PASS_FILE ))
+    logger.debug( "GLOBAL_PASS: '{}' like ethernet 'aabbccddeeff' ".format(GLOBAL_PASS))
+    logger.info( "Log File:{}".format( LOG_FILE ))
+    logger.info( "LOG LEVEL:{}".format( LOGLEVEL ))
+
+    try:
+        start_listener()
+    except KeyboardInterrupt:
+        logger.debug("Exiting because of keyboard interrupt")
+        sys.exit()
